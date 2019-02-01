26b05f9f02f5edffb20c8169838c6eca4a3e1613
caizhh <andy@wangjigroup.com>
2019-02-01 16:18:41 +0800

    dp

--- src/main/java/com/test/it/algorithms/DynamicPlanning.java
+++ src/main/java/com/test/it/algorithms/DynamicPlanning.java
@@ -16,9 +16,9 @@ public class DynamicPlanning {
         // 结果放到 maxW 中
         int maxW = Integer.MIN_VALUE;
         // 物品重量
-        int[] weight = {2,2,4,6,3};
+        int[] weight = {2,2,4,6,3,7,3};
         // 物品个数
-        int n = 5;
+        int n = 7;
         // 背包承受的最大重量
         int w = 9;
 
@@ -50,6 +50,7 @@ public class DynamicPlanning {
                     state[i][j + weight[i]] = true;
                 }
             }
+            printState(state, weight);
         }
 
         int maxW = 0;
@@ -60,19 +61,38 @@ public class DynamicPlanning {
             }
         }
 
+        //TODO 如何输出 选中的结果？
         int i = n-1;
         int j = maxW;
         List<Integer> picked = Lists.newArrayList();
         while(j > 0 && i > 0) {
-            if (state[i][j]) {
+            if (state[i][j] && j >= weight[i]) {
                 j -= weight[i];
                 picked.add(weight[i]);
-                i--;
             }
+            i--;
         }
         Lists.reverse(picked).forEach(pw -> System.out.print(pw + " "));
 
         return maxW;
+    }
+
+    private static void printState(boolean[][] state, int[] weight) {
+        System.out.print("  |");
+        for (int i = 0; i < state[0].length; i++) {
+            System.out.print(" " + i);
+        }
+        System.out.println("\n--+--------------------");
+        for (int i = 0; i < state.length; i++) {
+            for (int j = 0; j < state[i].length; j++) {
+                if (j == 0) {
+                    System.out.print(i + " | ");
+                }
+                System.out.print(state[i][j] ? "1 " : "0 ");
+            }
+            System.out.println(" --weight[" + i + "]=" + weight[i]);
+        }
+        System.out.println("\n");
     }
 
 
